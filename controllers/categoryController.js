1a2d52a0a7a96437d62fae8a1369b89845de0368
Raiyan109 <kraiyan109@gmail.com>
2023-10-26 07:24:19 +0600

    category deleted

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,23 +1,44 @@
 const Category = require('../models/categoryModel.js')
 
-
+// POST || Create
 const createCategory = async (req, res) => {
     const { name } = req.body
 
     try {
         const category = await Category.create({ name })
+        if (!category) {
+            return res.status(400).send({ msg: 'The category cannot be created' })
+        }
         res.status(200).json(category)
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
 }
 
+// GET all categories
 const getCategories = async (req, res) => {
     const categories = await Category.find({})
     res.status(200).json(categories)
 }
 
 
+// DELETE a category
+const deleteCategory = async (req, res) => {
+    try {
+        const category = await Category.findByIdAndRemove(req.params.id)
+        if (!category) {
+            return res.status(400).json({ msg: 'The category cannot be deleted' })
+        }
+        res.status(200).json({
+            success: true,
+            message: 'Successfully deleted',
+            category
+        })
+    } catch (error) {
+        res.status(400).json({ error: 'Error from catch ' + error.message })
+    }
+}
 
-module.exports = { createCategory, getCategories }
+
+module.exports = { createCategory, getCategories, deleteCategory }
 
--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const { createCategory, getCategories } = require('../controllers/categoryController.js')
+const { createCategory, getCategories, deleteCategory } = require('../controllers/categoryController.js')
 
 const router = express.Router()
 
 
 // routes
+// GET
 router.get('/', getCategories)
 
+// POST
 router.post('/', createCategory)
+
+// DELETE
+router.delete('/:id', deleteCategory)
 
 
 
